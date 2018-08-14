92634e5454b0b479d6c4e0ba377d631d2fd45712
cooper-mj <coopermj@stanford.edu>
2018-08-14 16:10:14 -0700

    Adding XCode files.

---
+++ Bridge Network Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bridge Network Test
+//
+//  Created by Michael Cooper on 2018-08-14.
+//  Copyright © 2018 Michael Cooper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
